45129b285a50c75ca687c938a6301b06ea747f84
zengxingqi <me@zengxingqi.com>
2020-03-15 14:14:31 +0800

    update: error memory

--- serve.js
+++ serve.js
@@ -7,4 +7,13 @@ if (cluster.isMaster) {
     for (idx; idx < len; idx++) cluster.fork();
 } else {
     require("./index.js");
+    // 如果使用 pm2 ，则忽略下面的代码
+    // process.on("uncaughtException", (err) => {
+    //     console.error(err);
+    //     process.exit(1);
+    // });
+    // 内存阀值/泄漏
+    // if (process.memoryUsage().rss > 734003200) {
+    //     process.exit(1);
+    // }
 };
