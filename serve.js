94d452feb0403124f6216a2fb8a80f152b486756
zengxingqi <me@zengxingqi.com>
2020-03-15 15:39:58 +0800

    add: nodejs cluster

--- serve.js
+++ serve.js
@@ -1,19 +1,58 @@
+
+// 如果使用 pm2 管理进程，则忽略此文件
 const cluster = require("cluster");
 const os = require("os");
+
+function fork() {
+    const work = cluster.fork();
+    let missedPing = 0;
+    let inter = setInterval(() => {
+        work.send("ping");
+        missedPing++;
+        if (missedPing >= 3) {
+            clear();
+            process.kill(work.process.pid);
+        };
+    }, 3000);
+    function clear() {
+        clearInterval(inter);
+        inter = null;
+        missedPing = null;
+    }
+    work.on("message", msg => {
+        if (msg == "pong") {
+            missedPing--;
+        };
+    });
+    work.on("exit", (code, signal) => {
+        clear();
+    });
+};
 
 if (cluster.isMaster) {
     const len = os.cpus().length / 2;
     let idx = 0;
-    for (idx; idx < len; idx++) cluster.fork();
+    for (idx; idx < len; idx++) fork();
+    cluster.on("exit", () => {
+        fork();
+    });
 } else {
     require("./index.js");
-    // 如果使用 pm2 ，则忽略下面的代码
-    // process.on("uncaughtException", (err) => {
-    //     console.error(err);
-    //     process.exit(1);
-    // });
-    // 内存阀值/泄漏
-    // if (process.memoryUsage().rss > 734003200) {
-    //     process.exit(1);
-    // }
+    process.on("message", msg => {
+        if (msg == "ping") {
+            process.send("pong");
+        };
+    });
+    process.on("uncaughtException", (err) => {
+        console.error("uncaughtException", err);
+        process.exit(1);
+    });
+    let inter = setInterval(() => {
+        console.log(process.memoryUsage().rss);
+        if (process.memoryUsage().rss > 30797824) {
+            clearInterval(inter);
+            inter = null;
+            process.exit(1);
+        };
+    }, 1000);
 };
